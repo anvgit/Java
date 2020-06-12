b2153c0785e3062e3eacf435fa7d09a6f90d6dc2
anvgit <47610732+anvgit@users.noreply.github.com>
2020-06-12 10:39:27 -0500

    Add files via upload

---
+++ IndexedProperty.java
@@ -0,0 +1,15 @@
+package com.anvesh.basic.beans;
+
+public class IndexedProperty {
+	private double data[];
+
+	public double[] getData() {
+		return data;
+	}
+
+	public void setData(double[] data) {
+		this.data = data;
+	}
+	
+	
+}

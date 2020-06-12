8f1cd425a3887e7ede002285d203f58312f3af82
anvgit <47610732+anvgit@users.noreply.github.com>
2020-06-12 10:41:38 -0500

    Add files via upload

---
+++ BRReadLines.java
@@ -0,0 +1,17 @@
+package com.anvesh.basic.io;
+
+import java.io.*;
+
+public class BRReadLines {
+	public static void main(String args[]) throws IOException {
+		String str;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter lines of text");
+		
+		do {
+			str = br.readLine();
+			System.out.println(str);
+		}while(!str.equals("stop"));
+	}
+}
